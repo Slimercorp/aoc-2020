d7d102a53ef9d12ebcecfbedfc0131f5cff03dff
Sergey Chekmarev <slimercorp@gmail.com>
2020-12-14 14:40:45 +0300

    Day 10 completed

--- Day10/Solve2.m
+++ Day10/Solve2.m
@@ -16,6 +16,20 @@ end
 sortedNumbers = sort(numbers);
 
 inputVoltage = 0;
-outputVoltage = sortedNumbers(end) + 3;
+outputVoltage = sortedNumbers(end);
 
 %%
+data = [1:outputVoltage]';
+data(:,2) = zeros(length(data(:,1)),1);
+data(1,2) = 1;
+data(2,2) = 2;
+data(3,2) = 4;
+
+for i=1:length(sortedNumbers)
+    if sortedNumbers(i) == 1 || sortedNumbers(i) == 2 || sortedNumbers(i) == 3
+        continue;
+    end
+    data(sortedNumbers(i),2) = data(sortedNumbers(i)-1,2) + data(sortedNumbers(i)-2,2) + data(sortedNumbers(i)-3,2);   
+end
+
+data(outputVoltage)
