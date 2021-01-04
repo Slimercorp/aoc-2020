fb285fd7c6984656f2eead787f39c3fe84c5b1f3
Sergey Chekmarev <slimercorp@gmail.com>
2021-01-04 20:34:35 +0300

    Day 22 Part 2 completed

--- Day22/Solve2.m
+++ Day22/Solve2.m
@@ -51,7 +51,7 @@ Player2 = [45
 40
 6];
   
-
+% 
 % Player1 = [
 % 9
 % 2
@@ -67,7 +67,8 @@ Player2 = [45
 % 7
 % 10];
 %%
-[winner, stack1Out, stack2Out] = getWinner(Player1, Player2);
+level = 1;
+[winner, stack1Out, stack2Out] = getWinner(Player1, Player2, level);
 
 sum = 0;
 lengthStack1=length(stack1Out);
@@ -85,38 +86,49 @@ if lengthStack2 ~= 0
 end
 
 %%
-function [winner, stack1Out, stack2Out] = getWinner(stack1, stack2)
-lastStack1 = zeros(length(stack1),1);
-lastStack2 = zeros(length(stack2),1);
-winner = -1;
+function [winner, stack1Out, stack2Out] = getWinner(stack1, stack2, level)
+level = level + 1;
+
 lengthStack1 = length(stack1);
 lengthStack2 = length(stack2);
+i = 1;
+lastStacks = cell(1);
+lastStacks{1}{1} = cell(1);
+lastStacks{1}{2} = cell(1);
+lastStacks{1}{1} = 0;
+lastStacks{1}{2} = 0;
 while 1
+    
+    if mod(i,100) == 0
+        clc;
+        disp(['round = ', num2str(i), ' level = ', num2str(level)]);
+    end
+    
+    
 %% Проверка на неизменность колод карт у Игрока 1 и Игрока 2    
-    theSame = true;
-    for i=1:length(stack1)
-        if lastStack1(i) ~= stack1(i)
-            theSame = false;
+    theSame = false;
+    for j=1:i
+        lastStack1 = lastStacks{j}{1};
+        lastStack2 = lastStacks{j}{2};
+        theSame = theSame || compareStacks(stack1,stack2,lastStack1,lastStack2);
+        if theSame
             break;
         end
     end
     
-    if theSame
-        for i=1:length(stack2)
-            if lastStack2(i) ~= stack2(i)
-                theSame = false;
-                break;
-            end
-        end
-    end
-    
-    if theSame
+    if theSame && i ~= 1
+        stack1Out = stack1;
+        stack2Out = stack2;
         winner = 1;
         break;
     end
-    
-    lastStack1 = stack1;
-    lastStack2 = stack2;
+        
+    i = i + 1;
+    lastStacks{i} = cell(1);
+    lastStacks{i}{1} = cell(1);
+    lastStacks{i}{2} = cell(1);
+    lastStacks{i}{1} = stack1;
+    lastStacks{i}{2} = stack2;
 
 %%
     playCard1 = stack1(1);
@@ -127,7 +139,7 @@ while 1
         newStack1 = stack1(2:playCard1+1);
         newStack2 = stack2(2:playCard2+1);
         
-        [winner,~,~]=getWinner(newStack1, newStack2);
+        [winner,~,~]=getWinner(newStack1, newStack2, level);
         
         if winner == 1
             player1Win = true;
@@ -144,9 +156,11 @@ while 1
         if playCard1 > playCard2
             player1Win = true;
             player2Win = false;
-        else
+        elseif playCard1 < playCard2
             player1Win = false;
             player2Win = true; 
+        else
+            error('Ошибка');
         end
     end
     
@@ -159,7 +173,7 @@ while 1
         stack2(1:lengthStack2-1) = stack2(2:lengthStack2);
         stack2(lengthStack2) = [];
         lengthStack2 = lengthStack2 - 1;
-    else
+    elseif player2Win
         stack2(1:lengthStack2-1) = stack2(2:lengthStack2);
         stack2(lengthStack2) = playCard2;
         stack2(lengthStack2+1) = playCard1;
@@ -168,6 +182,8 @@ while 1
         stack1(1:lengthStack1-1) = stack1(2:lengthStack1);
         stack1(lengthStack1) = [];
         lengthStack1 = lengthStack1 - 1;
+    else
+        error('Ошибка');
     end
     
     if lengthStack1 == 0
@@ -184,5 +200,29 @@ while 1
         break;
     end
 end
+    
+end
+
+%%
+function theSame = compareStacks(stack1,stack2,lastStack1,lastStack2)
+    theSame = true;
+    if length(stack1) ~= length(lastStack1) || length(stack2) ~= length(lastStack2)
+        theSame = false;
+        return;
+    end
+        
+    for i=1:length(stack1)
+        if lastStack1(i) ~= stack1(i)
+            theSame = false;
+            return;
+        end
+    end
+    
+    for i=1:length(stack2)
+        if lastStack2(i) ~= stack2(i)
+            theSame = false;
+            return;
+        end
+    end
     
 end
